8415f583a86b5bcce18d9e623206b1f342a5a74d
Rakibul Yeasin <rytotul@gmail.com>
2021-05-13 15:11:43 +0600

    Forked Yelp OSS site design

--- README.md
+++ README.md
@@ -1,26 +1,7 @@
-# My Projects
-### [TPaste](TPaste)   [![Build Status](https://travis-ci.org/rytotul/TPaste.svg?branch=master)](https://travis-ci.org/rytotul/TPaste)
-The package allows you to communicate directly with Pastebin.com from your Python application, either logged in or anonymously. This can be handy for a number of reasons - dumping error logs before an abort, regular web based status updates, but can’t be bothered getting a web-server running etc.
+# yelp.github.io
 
-### [SMS-TSH](SMS-TSH)   [![Build Status](https://travis-ci.org/rytotul/SMS-TSH.svg?branch=master)](https://travis-ci.org/rytotul/SMS-TSH)
-An api module in python to integrate with the DianaHost Bulk SMS service. This Allows you do evrything you would like to do on their website. But this is more fast and handy. It still in development process. It will be helpful if you want to help me by sharing codes.
+Source code for our opensource site showcasing all of our opensource projects.
 
-### [iAmLazy](iAmLazy)   [![Build Status](https://travis-ci.org/rytotul/iAmLazy.svg?branch=master)](https://travis-ci.org/rytotul/iAmLazy)
-I am too much lazy and also love to experiment with code
-or my system. So, the system usually goes wrong and I need to
-reinstall it. But after the installing process finishes my job starts.
-I always have to customize and install the pachages mannually.
-And it takes time from the reserved time for my sleep.
+## License
 
-_So, I somehow managed to write the scripts and help me to be lazy_
-
-### [FM Radio Live Stream (Bangladesh Only)](bd-fm-radio)
-I love to listen songs while working. And each time I start to search for a music on my phone there is nothing. So, I grabbed url's of some popular Stations and merged them all together. It's fun to me...
-
-### [Youtube To MP3](Youtube2mp3)
-It has a story behind it. Ignore that!
-Basically it is for downloading the mp3 version of some musics hosted on [YouTube](https://youtube.com/). What the script does is it grabs the page url and sends it to an converter. Then the converter processes the media and generates a download link for mp3. The script again fetches the download link and generates a QR code for me. I simply scan the code and Whoalla! The Music starts to download...
-
-# My Contributions
-### [BookBucket](https://github.com/sakib-rahman-bangladesh/bookbucket)
-The project was like a book-sharing platform. Somehow the developer failed to gain his target and closed it. But I am waiting for re-opening.
+This is licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
---
+++ js/projects.js
@@ -0,0 +1,89 @@
+oss_projects = {};
+oss_projects['dockersh'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2014/08/hack209-dockersh.html'
+}
+oss_projects['osxcollector'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/01/osxcollector-forensic-collection-and-automated-analysis-for-os-x.html',
+};
+oss_projects['paasta'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/11/introducing-paasta-an-open-platform-as-a-service.html',
+  featured: true,
+  position: 1,
+  background: 'paasta_bg.png'
+};
+oss_projects['elastalert'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/10/elastalert-alerting-at-scale-with-elasticsearch.html',
+  featured: true,
+  position: 2,
+  background: 'elastalert.png'
+}
+oss_projects['mrjob'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2010/10/mrjob-distributed-computing-for-everybody.html'
+}
+oss_projects['yelp-ruby'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2014/04/more-yelp-in-your-ruby.html'
+};
+oss_projects['firefly'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2012/08/firefly-illuminate-your-websites-performance.html',
+};
+oss_projects['pyleus'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2014/10/introducing-pyleus.html',
+};
+oss_projects['MOE'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2014/07/introducing-moe-metric-optimization-engine-a-new-open-source-machine-learning-service-for-optimal-ex.html'
+}
+oss_projects['fullerite'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/10/fullerite-a-new-mineral-to-collect-metrics.html'
+}
+oss_projects['mycroft'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/04/mycroft-load-data-into-redshift-automatically.html'
+}
+oss_projects['YLTableView'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/06/advanced-uitableviews-made-simple-yltableview.html'
+}
+oss_projects['amira'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2016/08/amira-automated-malware-incident-response-and-analysis.html'
+}
+oss_projects['dumb-init'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2016/01/dumb-init-an-init-for-docker.html',
+  featured: true,
+  position: 4,
+  background: 'dumb-init.png'
+}
+oss_projects['data_pipeline'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2016/07/billions-of-messages-a-day-yelps-real-time-data-pipeline.html'
+}
+oss_projects['mysql_streamer'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2016/08/streaming-mysql-tables-in-real-time-to-kafka.html'
+}
+oss_projects['schematizer'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2016/08/more-than-just-a-schema-store.html'
+}
+oss_projects['beans'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2017/02/open-sourcing-yelp-beans.html'
+}
+oss_projects['love'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2017/02/open-sourcing-yelp-love.html'
+}
+oss_projects['undebt'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2016/08/undebt-how-we-refactored-3-million-lines-of-code.html',
+  featured: true,
+  position: 3,
+  background: 'undebt.png'
+}
+
+
+oss_projects['zygote'] = { deprecated: true }
+oss_projects['s3mysqldump'] = { deprecated: true }
+oss_projects['kegmate'] = { deprecated: true }
+oss_projects['doloop'] = { deprecated: true }
+oss_projects['sqlite3dbm'] = { deprecated: true }
+oss_projects['send_nsca'] = { deprecated: true }
+oss_projects['mr3po'] = { deprecated: true }
+oss_projects['exio'] = { deprecated: true }
+oss_projects['dateglob'] = { deprecated: true }
+oss_projects['PushmasterApp'] = { deprecated: true }
+oss_projects['thehub'] = { deprecated: true }
+oss_projects['SWFUpload'] = { deprecated: true }
+
+// vim: sw=2 sts=2 expandtab
